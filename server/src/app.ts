c27f1998597d669f8aa8ea6ca12d1f1d8dacbd6e
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-09-28 02:25:24 +0200

    feat(server): Added helmet and cors modules and prepared example endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Express !');
+app.get('/api/example', (req: Request, res: Response) => {
+  res.json({ msg: 'Hello From Backend !' });
 });
 
 app.listen(port, () => {
