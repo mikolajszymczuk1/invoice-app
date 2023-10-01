6cca4d5ca9cce58288973b0cfe793b989d309a67
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-10-01 19:44:30 +0200

    fix(server) added response code to example endpoint

--- server/src/router/api.ts
+++ server/src/router/api.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 const api = express.Router();
 
 api.get('/example', (req: Request, res: Response) => {
-  res.json({ msg: 'Hello From Backend !' });
+  res.json({ msg: 'Hello From Backend !' }).status(200);
 });
 
 export default api;
