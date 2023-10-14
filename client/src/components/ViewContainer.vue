faf73e0d62da828c690a71fa70f7ed5f1c181dc5
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-10-14 22:42:08 +0200

    fix(client): fixed padding for ViewContainer component

--- client/src/components/ViewContainer.vue
+++ client/src/components/ViewContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto px-[24px] pt-[32px] pb-[105px] md:px-[48px] md:pt-[56px] md:pb-[173px] lg:pt-[72px] lg:w-[730px]">
+  <div class="mx-auto px-[24px] pt-[32px] pb-[105px] md:px-[48px] md:pt-[56px] md:pb-[173px] lg:pt-[72px] lg:px-0 lg:w-[730px]">
     <slot></slot>
   </div>
 </template>
