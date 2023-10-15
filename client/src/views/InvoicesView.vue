cceb237723c0d520fc4427e3f961a8c04b4a75b6
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-10-15 14:05:51 +0200

    Created There is nothing component

--- client/src/views/InvoicesView.vue
+++ client/src/views/InvoicesView.vue
@@ -1,12 +1,20 @@
 <template>
   <ViewContainer>
     <FilterSection />
-    <HelloWorld />
+
+    <div class="flex flex-col items-center mt-[102px] text-center font-spartan md:mt-[210px]">
+      <NothingIllustration class="md:w-[241px] md:h-auto" />
+      <h2 class="mt-[40px] mb-[24px] text-blue-dark text-[1.25rem] font-bold md:mt-[64px]">There is nothing here</h2>
+      <h3 class="text-gray-medium text-[.75rem]">
+        Create an invoice by clicking the <br>
+        <b>New</b> button and get started
+      </h3>
+    </div>
   </ViewContainer>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
 import FilterSection from '@/widgets/FilterSection.vue';
 import ViewContainer from '@/components/ViewContainer.vue';
+import NothingIllustration from '@/components/illustrations/NothingIllustration.vue';
 </script>
